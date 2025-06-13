10121843424e5367c222a88924f67d5ea9afa7ab
engrsakib <snazmussakib01@gmail.com>
2025-06-14 04:23:14 +0600

    navbar make it stky

--- src/components/personal/Footer.tsx
+++ src/components/personal/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
+    <div className='text-[20px] font-[400] ml-4 bg-gradient-to-r from-[#7F0B0B] to-[#590000] p-8 text-white px-4 py-2 rounded w-full min-h-[250px] flex items-center justify-center hover:transition-all duration-300'>
       
     </div>
   )
