c2de9232d02aaeed72e403611b9146e909d56aec
engrsakib <snazmussakib01@gmail.com>
2025-06-15 00:17:24 +0600

    banner section update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="max-w-full items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       {/* headding */}
-      <div className="mt-[202px]">
+      <div className="mt-[52px]">
         <Title
           heading="Ace Your PTE Core Exam"
           subheading="with AI-Powered Practice!"
